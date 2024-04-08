81046d6a5deb273fd87bc40a8bf396f41d022f92
Cheuk Lee <34779510+leecheuk@users.noreply.github.com>
2024-04-07 20:35:16 -0400

    styles: add JSDocs (#19)

--- src/numeric.ts
+++ src/numeric.ts
@@ -1,18 +1,49 @@
 import { Message, MixedSchema, Reference } from "yup";
-import { isAbsent, isEqual, isGreaterThan, isGreaterThanOrEqual, isInteger, isLessThan, isLessThanOrEqual, isLessThanWithRef, isMoreThanWithRef } from "./utils";
+import {
+  isAbsent,
+  isEqual,
+  isGreaterThan,
+  isGreaterThanOrEqual,
+  isInteger,
+  isLessThan,
+  isLessThanOrEqual,
+  isLessThanWithRef,
+  isMoreThanWithRef,
+} from "./utils";
 import BigNumber from "bignumber.js";
 
 export interface INumericSchema extends MixedSchema<string> {
-  gte: (num: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
-  lte: (num: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
-  eq: (num: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
-  gt: (num: number|string, message?: Message<any>) => INumericSchema;
-  lt: (num: number|string, message?: Message<any>) => INumericSchema;
-  min: (num: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
-  max: (num: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
+  gte: (
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
+  lte: (
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
+  eq: (
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
+  gt: (num: number | string, message?: Message<any>) => INumericSchema;
+  lt: (num: number | string, message?: Message<any>) => INumericSchema;
+  min: (
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
+  max: (
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
   integer: (message?: Message<any>) => INumericSchema;
-  moreThan: (more: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
-  lessThan: (more: number|string|Reference<number|string>, message?: Message<any>) => INumericSchema;
+  moreThan: (
+    more: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
+  lessThan: (
+    more: number | string | Reference<number | string>,
+    message?: Message<any>
+  ) => INumericSchema;
   required: (message?: Message<any>) => INumericSchema;
   optional: () => INumericSchema;
   nullable: (message?: Message<any>) => INumericSchema;
@@ -22,10 +53,10 @@ export interface INumericSchema extends MixedSchema<string> {
 export class NumericSchema extends MixedSchema<string> {
   constructor() {
     super({
-      type: 'numeric',
+      type: "numeric",
       check(value: string): value is NonNullable<string> {
         const parsed = new BigNumber(value);
-        return typeof value === 'string' && !parsed.isNaN();
+        return typeof value === "string" && !parsed.isNaN();
       },
     });
 
@@ -33,69 +64,157 @@ export class NumericSchema extends MixedSchema<string> {
       schema.transform(function mutate(value) {
         if (isAbsent(value)) return value;
         return value.toString();
-      })
+      });
     });
   }
 
-  gte(num: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
+  /**
+   * Value must be greater than or equal to `num` (ie. value >= num),
+   * where `num` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  gte(
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
     return isGreaterThanOrEqual.call(this, num, message);
   }
 
-  lte(num: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
+  /**
+   * Value must be less than or equal to `num` (ie. value <= num),
+   * where `num` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  lte(
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
     return isLessThanOrEqual.call(this, num, message);
   }
 
-  min(num: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
-    return isGreaterThanOrEqual.call(this, num, message);
+  /**
+   * Set a minimum value for the string value (ie. value >= min),
+   * where `min` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  min(
+    min: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
+    return isGreaterThanOrEqual.call(this, min, message);
   }
 
-  max(num: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
-    return isLessThanOrEqual.call(this, num, message);
+  /**
+   * Set a maximum value for the string value (ie. value <= max),
+   * where `max` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  max(
+    max: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
+    return isLessThanOrEqual.call(this, max, message);
   }
 
-  eq(num: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
+  /**
+   * Value must be equal to the given number `num` (ie. value === num),
+   * where `num` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  eq(
+    num: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
     return isEqual.call(this, num, message);
   }
 
-  gt(num: number|string, message?: Message<any>): INumericSchema {
-    return isGreaterThan.call(this, num, message);
+  /**
+   * Value must be more than the given number `more` (ie. value > more),
+   * where `more` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  gt(more: number | string, message?: Message<any>): INumericSchema {
+    return isGreaterThan.call(this, more, message);
   }
 
-  lt(num: number|string, message?: Message<any>): INumericSchema {
-    return isLessThan.call(this, num, message);
+  /**
+   * Value must be less than the given number `less` (ie. value < less),
+   * where `less` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  lt(less: number | string, message?: Message<any>): INumericSchema {
+    return isLessThan.call(this, less, message);
   }
 
+  /**
+   * Value must be an integer.
+   */
   integer(message?: Message<any>): INumericSchema {
     return isInteger.call(this, message);
   }
 
-  moreThan(more: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
+  /**
+   * Value must be more than the given number `more` (ie. value > more),
+   * where `more` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  moreThan(
+    more: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
     return isMoreThanWithRef.call(this, more, message);
   }
 
-  lessThan(less: number|string|Reference<number|string>, message?: Message<any>): INumericSchema {
+  /**
+   * Value must be less than the given number `less` (ie. value < less),
+   * where `less` can be a string, number or reference to another field.
+   * For instance, `ref('field_name')`.
+   */
+  lessThan(
+    less: number | string | Reference<number | string>,
+    message?: Message<any>
+  ): INumericSchema {
     return isLessThanWithRef.call(this, less, message);
   }
 
+  /**
+   * Value is required.
+   */
   required(msg?: Message<any>): INumericSchema {
     return super.required(msg) as INumericSchema;
   }
 
+  /**
+   * Value is not required.
+   */
   notRequired(msg?: Message<any>): INumericSchema {
     return super.required(msg) as INumericSchema;
   }
 
+  /**
+   * Value is optional.
+   */
   optional(): INumericSchema {
     return super.optional() as INumericSchema;
   }
 
+  /**
+   * Value cannot be null.
+   */
   nonNullable(): INumericSchema {
     return super.nonNullable() as INumericSchema;
   }
 
+  /**
+   * Value can be null.
+   */
   nullable(msg?: Message<any> | undefined): INumericSchema {
     return super.nullable(msg) as INumericSchema;
   }
 }
 
+/**
+ * yup schema for numeric string. Schema is used to validate strings that only contain
+ * numbers which can be float or integer.
+ */
 export const numeric = () => new NumericSchema();
